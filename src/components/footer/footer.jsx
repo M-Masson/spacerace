5ddcd1e910e6571a56ab05b16220bc75c84c738d
Masson <morgan.masson006@gmail.com>
2024-12-05 17:17:18 +0100

    modif css footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,14 +6,16 @@ import faceBook from '../../assets/svg/facebook.svg'
 import logoX from '../../assets/svg/x.svg'
 import instagram from '../../assets/svg/instagram.svg'
 
-const pages = [
+const column1 = [
     {title: "ESA", path: ""},
-    {title: "Blue Origin", path:""},
     {title: "I Space", path:""},
-    {title: "Prométhée", path:""},
     {title: "Space X", path:""},
+    {title: "Gama", path:""}
+]
+const column2 = [
+    {title: "Blue Origin", path:""},
+    {title: "Prométhée", path:""},
     {title: "Nasa", path:""},
-    {title: "Gama", path:""},
     {title: "Moon Express", path:""}
 ]
 
@@ -22,25 +24,34 @@ function Footer(){
         <footer>
             <div id='footer'>
                 <div id='footer-p1'>
-                    <img src={logoTypo} alt="logo typo" />
+                    <img src={logoTypo} alt="logo typo" id='logo-space-race-footer' />
                     <p id='footer-txt'> Ce site internet vous permet de vous
                         informer sur les dernières actualités de
                         la course à l'espace.
                     </p>
                     <div id='cielEspace'>
                         <p>Site satellite de</p>
-                        <a href=""><img src={logoCiel} alt="ciel & espace" /></a>
+                        <a href=""><img src={logoCiel} alt="ciel & espace" id='logo-ciel-espace' /></a>
                     </div>
 
                 </div>
                 <div id='footer-p2'>
                     <p>Acteurs</p>
                     <div id='footer-list'>
-                        {pages.map((item, index)=>
-                            <Link key={index} to="">
-                                {item.title}
-                            </Link>
-                        )}
+                        <div id='list-part1'>
+                            {column1.map((item, index)=>
+                                <Link key={index} to="">
+                                    {item.title}
+                                </Link>
+                            )}
+                        </div>
+                        <div id='list-part2'>
+                            {column2.map((item, index)=>
+                                <Link key={index} to="">
+                                    {item.title}
+                                </Link>
+                            )}
+                        </div>
                     
                     </div>
                 </div>
